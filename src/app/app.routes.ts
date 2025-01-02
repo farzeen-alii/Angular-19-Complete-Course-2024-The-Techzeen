2fb9daf2fad17a6a57a071696153ce6788f603d2
farzeen-alii <techzeen10@gmail.com>
2025-01-02 22:38:51 +0500

    Testing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' }, // Default Route
+  { path: 'home', component: HomeComponent },           // Home Route
+  { path: 'about', component: AboutComponent },
+];
